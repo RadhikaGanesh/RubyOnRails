12a368db60d643c48b02d7ecd2ffc15a03bfda62
Radhika <radhikag995@gmail.com>
2016-07-06 18:17:12 +0530

    added code for deleting comments & associated models

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,7 +1,14 @@
 class Article < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
-  validates :text, presence: true,
-                    length: { minimum: 5 }
+ 
+   before_save :send_text
+  def send_text
+  	self.text = self.title if text.blank?
+  end
+  after_save :send_message
+  def send_message
+		puts 'saved successfully' 
+  end
 end
